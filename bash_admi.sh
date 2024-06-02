2f6826632ec06a119d614047c21a12e175f53cad
BernalCamilo <jc_bernal2011@hotmail.com>
2024-06-02 15:42:25 -0500

    feat (bash-powershell-processes): Display name and size of the largest file stored on a disk or filesystem

--- bash_admi.sh
+++ bash_admi.sh
@@ -4,7 +4,14 @@ get_consumming_processes () {
     ps aux --sort %cpu | head -6
 }
 
+get_largest_file () {
+    read -p "Ingrese el path del disco o filesystem: " path
+    find "$path" -type f -exec ls -s {} + | sort -n -r | head -n 1
+}
+
+
 print_menu () {
+    clear
     echo "1. Show the 5 processes that are consuming the most CPU"
     echo "2. Display the connected filesystems and disks"
     echo "3. Get the largest file in a filesystem or disk"
@@ -17,28 +24,15 @@ execute_operation () {
     #$1 is the operation to perform. Int, possible values 1,2,3,4,5,9
     (( operation = $1 ))
     case $operation in
-        1)
-            get_consumming_processes
-        ;;
-        2)
-            echo "2"
-        ;;
-        3)
-            echo "3"
-        ;;
-        4)
-            echo "4"
-        ;;
-        5)
-            echo "5"
-        ;;
-        9)
-            echo "Admin session finished successfully."
-        ;;
-        *)
-            echo "Not a valid option. Try again."
-        ;;
+        1)  get_consumming_processes;;
+        2)  echo "2";;
+        3)  get_largest_file;;
+        4)  echo "4";;
+        5)  echo "5";;
+        9)  echo "Admin session finished successfully.";;
+        *)  echo "Not a valid option. Try again.";;
     esac
+	read -p "Presione cualquier tecla para continuar..." -n1 -s
 }
 
 (( option=0 ))
