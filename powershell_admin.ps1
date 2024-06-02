b772d6f0790e5051b9d380087209dc3e60ac66b3
Julian Bolaños <julian.camilob24@gmail.com>
2024-06-02 11:55:04 -0500

    feat (powershell-menu): Implemented menu print and operation execution. Add menu with 5 options and a function to execute each according to a user input.

--- powershell_admin.ps1
+++ powershell_admin.ps1
@@ -1,1 +1,48 @@
-#Empty script
+function Print-Menu {
+    Write-Output "1. Show the 5 processes that are consuming the most CPU"
+    Write-Output "2. Display the connected filesystems and disks"
+    Write-Output "3. Get the largest file in a filesystem or disk"
+    Write-Output "4. Get empty memory space and percentage of used swap space"
+    Write-Output "5. Get number of active network connections"
+    Write-Output "9. Exit program"
+}
+
+function Execute-Operation {
+    param (
+        [int]$Operation
+    )
+    switch ($Operation) {
+        1 {
+            Write-Output "1"
+        }
+        2 {
+            Write-Output "2"
+        }
+        3 {
+            Write-Output "3"
+        }
+        4 {
+            Write-Output "4"
+        }
+        5 {
+            Write-Output "5"
+        }
+        9 {
+            Write-Output "Admin session finished successfully."
+        }
+        default {
+            Write-Output "Not a valid option. Try again."
+        }
+    }
+}
+
+$option = 0
+while ($option -ne 9) {
+    Print-Menu
+    $input = Read-Host "Enter your option"
+    if ([int]::TryParse($input, [ref]$option)) {
+        Execute-Operation -Operation $option
+    } else {
+        Write-Output "Not a valid option. Try again."
+    }
+}
